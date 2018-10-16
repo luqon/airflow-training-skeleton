e8c45c3204befb3bb4a8992f928468aaad925a27
Ernst Kuiper <ekuiper@bol.com>
2018-10-16 14:44:25 +0200

    Remove deleting of data

--- other/build_statistics_simple.py
+++ other/build_statistics_simple.py
@@ -62,6 +62,5 @@ aggregation = spark.sql(
 
 (
     aggregation.write.mode("overwrite")
-    .partitionBy("transfer_date")
-    .parquet("gs://airflow_training_data/average_prices/")
+    .parquet("gs://airflow_training_data/average_prices/transfer_date={}/".format(dt))
 )
