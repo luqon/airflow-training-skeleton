39eff60ae36ea17489850f3a3723f60ad5f9317c
Ernst Kuiper <ekuiper@bol.com>
2018-10-17 11:14:29 +0200

    commit

--- dags/dag.py
+++ dags/dag.py
@@ -81,11 +81,12 @@ bucket_to_bq = GoogleCloudStorageToBigQueryOperator(
 )
 
 endpoint="https://europe-west1-gdd-airflow-training.cloudfunctions.net/airflow-training-transform-valutas?date=1970-01-01&from=GBP&to=EUR" #noqa: E501
-bla = HttpToGcsOperator(endpoint=endpoint,
-                        bucket="airflow_training_data",
-                        bucket_path="currencies/{{ds_nodash}}_{from_currency}_{to_currency}.json",
-                        dag=dag
-                        )
+bla = HttpToGcsOperator(
+    endpoint=endpoint,
+    bucket="airflow_training_data",
+    bucket_path="currencies/{{ds_nodash}}_GB_EUR.json",
+    dag=dag
+)
 
 pgsl_to_gcs >> dataproc_create_cluster >> compute_aggregates >> dataproc_delete_cluster
 compute_aggregates >> bucket_to_bq
--- dags/httptogcs_operator.py
+++ dags/httptogcs_operator.py
@@ -6,7 +6,7 @@ from airflow.contrib.hooks.gcs_hook import GoogleCloudStorageHook
 class HttpToGcsOperator(BaseOperator):
 
     template_fields = ('endpoint', 'bucket', 'bucket_path')
-    def __init__(self, endpoint, bucket, bucket_path, method='GET'):
+    def __init__(self, endpoint, bucket, bucket_path, method='GET', *args, **kwargs):
         self.endpoint = endpoint
         self.bucket = bucket
         self.bucket_path = bucket_path
@@ -14,15 +14,7 @@ class HttpToGcsOperator(BaseOperator):
 
     def execute(self):
         http_hook = HttpHook()
-        response = http_hook.run(self.endpoint, self,method)
+        response = http_hook.run(self.endpoint, self.method)
 
         #gcs_hook = GoogleCloudStorageHook()
         #gcs_hook.upload_file(response, self.bucket, self.bucket_path)
-
-endpoint='https://europe-west1-gdd-airflow-training.cloudfunctions.net/airflow-training-transform-valutas'
-
-bla = HttpToGcsOperator(endpoint=endpoint,
-                        bucket="airflow_training_data",
-                        bucket_path="currencies/{{ds_nodash}}_{from_currency}_{to_currency}.json"
-                        )
-bla.execute()
